7ae96c615371d4df4edf1be7305c3cb1c0a129e3
yuan77 <sky58029@gmail.com>
2017-09-18 15:16:15 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("學號:106022219");
+        System.out.println("系級：行動商務與多媒體應用學系一年級\n");
+        System.out.println("姓名：陳玟媛\n");
+        System.out.println("時間：2017/09/18\n");
+        System.out.print("地點：\"I319\"");
+	// write your code here
+    }
+}
